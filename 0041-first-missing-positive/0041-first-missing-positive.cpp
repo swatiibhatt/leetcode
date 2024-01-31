152da40c872989af2b0754ce35ee53f2534af00c
Swati Bhatt <swattidccd@gmail.com>
2024-01-31 16:08:38 +0530

    Time: 58 ms (33.56%), Space: 44.7 MB (20.75%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstMissingPositive(std::vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int current = 1;  
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                if (nums[i] == current) {
+                     while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+                        i++;  // Skip duplicates
+                    }
+                    current++;
+                } else {
+                    return current;
+                }
+            }
+        }
+       // current+=1;
+        return current;
+    }
+};
+
+
