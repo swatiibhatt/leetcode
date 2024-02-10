b84a678fa3fe2d3ff4e7b92a4570a4ea3f47abb9
Swati Bhatt <swattidccd@gmail.com>
2024-02-10 17:37:31 +0530

    Time: 10 ms (45.78%), Space: 15.5 MB (26.47%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,13 +1,40 @@
+// class Solution {
+// public:
+//     int countSubstrings(string S) {
+//         int len = S.length(), ans = 0;
+//         for (int i = 0; i < len; i++) {
+//             int j = i - 1, k = i;
+//             while (k < len - 1 && S[k] == S[k+1]) k++;
+//             ans += (k - j) * (k - j + 1) / 2, i = k++;
+//             while (~j && k < len && S[k++] == S[j--]) ans++;
+//         }
+//         return ans;
+//     }
+// };
+   #include <string>
+using namespace std;
+
 class Solution {
 public:
-    int countSubstrings(string S) {
-        int len = S.length(), ans = 0;
-        for (int i = 0; i < len; i++) {
-            int j = i - 1, k = i;
-            while (k < len - 1 && S[k] == S[k+1]) k++;
-            ans += (k - j) * (k - j + 1) / 2, i = k++;
-            while (~j && k < len && S[k++] == S[j--]) ans++;
+    int count = 0;
+    
+    int countSubstrings(string s) {
+        if (s == "" || s.length() == 0) return 0;
+        
+        for (int i = 0; i < s.length(); i++) { // i is the mid point
+            extendPalindrome(s, i, i); // odd length;
+            extendPalindrome(s, i, i + 1); // even length
         }
-        return ans;
+        
+        return count;
+    }
+    
+private:
+    void extendPalindrome(string s, int left, int right) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            count++; 
+            left--; 
+            right++;
+        }
     }
 };
